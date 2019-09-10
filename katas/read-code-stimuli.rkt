8e957612adde32752783501e37cff692db11602e
Dimitri Collas <dimitricollas@gmail.com>
2019-09-10 11:48:54 -0700

    changed kata page

--- katas/read-code-stimuli.rkt
+++ katas/read-code-stimuli.rkt
@@ -7,6 +7,9 @@
 (define stimuli
   (list
     `1-easy
-    (read "Make 10 into a roman numeral")
+    (read "Make 10 into a Roman numeral")
+
+    `2-easy
+    (read "Make Roman numeral 10 which is x into a regular number")
 
     ))
